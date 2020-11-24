d9391e38e8311f88a8bc519d52072ee4d61b307f
Bono Zrakić <bzrakic@koncar-institut.hr>
2020-11-24 17:33:07 +0100

    Nakon lekcije 11
    - dodano spremanje podataka u tekst datoteku
    - kreirana pomocna klasa sa funkcijama nad stringom itd
    koja se koristi samo kad je se pozove
    -

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
-        /// TODO - Make the CreatePrize method actually save to the text file
+        private const string PrizesFile = "PrizeModels.csv";
+        
         /// <summary>
         /// Saves a new prize to the text file
         /// </summary>
@@ -17,7 +19,25 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load the text file and Ccnvert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the max ID
+            int currentId = 1;
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            // Add the new record with the new ID (max + 1)
+            prizes.Add(model);
+
+            // Convert the prizes to list<string>
+            // Save the list<string> to the text file
+
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
